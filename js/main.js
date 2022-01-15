86cae4386bd8e1e19424ff363092d0692ac4cbb0
rostislav-nikitin90 <rosnikitin90@gmail.com>
2022-01-16 00:55:32 +0100

    Added Week02 page to portfolio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+document.querySelector('#year').textContent = (new Date).getFullYear();
+document.querySelector('#lastmodified').textContent = document.lastModified;
+
 const links = [
     {
       label: "Week01",
-      url: "week1/index.html"
+      url: "week1/index.html",
+
+      label: "Week02",
+      url: "week2/index.html"
     }
   ]
               
